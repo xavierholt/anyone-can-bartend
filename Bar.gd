e3e999293c33cd097cc85318f6ef857334ac8a26
Kevin Burk <xavierholt@gmail.com>
2019-02-20 12:25:16 -0800

    Timers.

--- Bar.gd
+++ Bar.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var Recipe = load("res://Recipe.gd")
+var Order  = load("res://Order.tscn")
 
 var pouring = false
 var source  = null
@@ -9,20 +10,23 @@ var cursor  = null
 
 var recipes = [
 	Recipe.new("Negroni", {
-		"gin":      3.0,
-		"campari":  3.0,
-		"vermouth": 3.0
+		"gin":            3.0,
+		"campari":        3.0,
+		"sweet vermouth": 3.0
 	}),
 	Recipe.new("Americano", {
-		"campari":    3.0,
-		"vermouth":   3.0,
-		"soda-water": 1.0
+		"campari":        3.0,
+		"sweet vermouth": 3.0,
+		"soda water":     1.0
 	})
 ]
 
 func _ready():
 	cursor = get_node("Cursor")
 	print(OS.get_screen_size())
+	
+	get_node("Timers/OrderTimer").connect("timeout", self, "tick")
+	get_node("Timers/LevelTimer").connect("timeout", self, "done")
 
 func _physics_process(delta):
 	if pouring: source.pour(target)
@@ -45,20 +49,13 @@ func click(slot):
 func unclick(slot):
 	pouring = false
 
-func deliver(order):
-	var drinks = []
-	var s = get_node("ServeTray")
-	for slot in s.get_children():
-		var i = slot.get_node("Click").item()
-		if i != null: drinks.append(i)
-	if len(drinks) == 0:
-		print("No drinks to deliver!")
-		return
-	if len(drinks) != 1:
-		print("Only deliver one drink at at time!")
-		return
-	for k in drinks[0].fluids:
-		print(k + ": " + str(drinks[0].fluids[k]))
-	var score = order.recipe.score(drinks[0])
-	print(order.recipe.name + ": " + str(score) + " stars")
-	drinks[0].queue_free()
+func done():
+	print("Level complete!")
+	get_tree().quit()
+
+func tick():
+	if randf() < 0.2:
+		print("Making an order...")
+		var o = Order.instance()
+		var v = get_node("MarginContainer/VBoxContainer")
+		v.add_child(o)
--- Order.gd
+++ Order.gd
@@ -1,11 +1,46 @@
 extends Button
 
 var recipe
+var remaining = 10
+var threshold = 5
 
 func _ready():
-	var recipes = get_node("/root/Bar").recipes
+	var bar   = get_node("/root/Bar")
+	var timer = bar.get_node("Timers/OrderTimer")
+	timer.connect("timeout", self, "tick")
+
+	var recipes = bar.recipes
 	recipe = recipes[randi() % len(recipes)]
-	set_text("1x " + recipe.name)
+	set_text(str(remaining) + ": 1x " + recipe.name)
 
 func _pressed():
-	get_node("/root/Bar").deliver(self)
+	fulfill()
+
+func fulfill():
+	var s = get_node("/root/Bar/ServeTray")
+	var drinks = []
+	for slot in s.get_children():
+		var i = slot.get_node("Click").item()
+		if i != null: drinks.append(i)
+	if len(drinks) == 0:
+		print("No drinks to deliver!")
+		return
+	if len(drinks) != 1:
+		print("Only deliver one drink at at time!")
+		return
+	for k in drinks[0].fluids:
+		print(k + ": " + str(drinks[0].fluids[k]))
+	var score = recipe.score(drinks[0])
+	print("Order fulfilled! :)")
+	print(" - " + recipe.name + ": " + str(score) + " stars")
+	drinks[0].queue_free()
+	queue_free()
+
+func tick():
+	if remaining <= 0:
+		#TODO: Complain!
+		print("Order timed out! :(")
+		queue_free()
+		return
+	remaining -= 1
+	set_text(str(remaining) + ": 1x " + recipe.name)
